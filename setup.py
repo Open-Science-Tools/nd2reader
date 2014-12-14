06f5b854a53cffdf568223189500945cebf8bee3
Jim Rybarski <jim@rybarski.com>
2014-12-14 13:13:21 -0600

    added setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nd2reader",
+    packages=find_packages(),
+    version="0.9.0",
+    install_requires=[
+        'numpy',
+    ],
+)
