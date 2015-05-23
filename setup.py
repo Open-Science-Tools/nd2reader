f3ebf77ddc7d290b475aa8f44babc93cc54d46b5
Jim Rybarski <jim@rybarski.com>
2015-05-23 19:14:16 +0000

    resolves #26

--- setup.py
+++ setup.py
@@ -3,5 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="nd2reader",
     packages=find_packages(),
-    version="0.9.7"
+    version="1.0.0",
+    description='A tool for reading ND2 files produced by NIS Elements',
+    author='Jim Rybarski',
+    author_email='jim@rybarski.com',
+    url='https://github.com/jimrybarski/nd2reader',
+    download_url='https://github.com/jimrybarski/nd2reader/tarball/1.0.0',
+    keywords=['nd2', 'nikon', 'microscopy', 'NIS Elements'],
+    classifiers=[],
 )
