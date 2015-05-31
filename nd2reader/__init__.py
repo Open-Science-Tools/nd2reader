0f9c6128e3af7cfbb738c27e745d049bea15db0f
Jim Rybarski <jim@rybarski.com>
2015-05-31 04:00:11 +0000

    #39: field of view works

--- nd2reader/__init__.py
+++ nd2reader/__init__.py
@@ -84,13 +84,13 @@ class Nd2(Nd2Parser):
                 return image
 
     def _calculate_field_of_view(self, frame_number):
-        return frame_number - (frame_number % (len(self.z_levels) + len(self.channels)))
+        return int((frame_number - (frame_number % (len(self.z_levels) * len(self.channels)))) / (len(self.z_levels) * len(self.channels)))
 
     def _calculate_channel(self, frame_number):
         return self._channels[frame_number % len(self.channels)]
 
     def _calculate_z_level(self, frame_number):
-        return self.z_levels[(frame_number * len(self.channels)) % len(self.z_levels) - frame_number % len(self.channels)]
+        return self.z_levels[int(((frame_number - (frame_number % len(self.channels))) / 2) % len(self.z_levels))]
 
     @property
     def image_sets(self):
