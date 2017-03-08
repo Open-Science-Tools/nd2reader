f94f70e3a8dcc6c5aacd7effd8fa27354ab99688
Ruben Verweij <verweij@physics.leidenuniv.nl>
2017-03-08 09:59:04 +0100

    initial documentation

--- nd2reader/__init__.py
+++
@@ -1,3 +0,0 @@
-from nd2reader.reader import ND2Reader
-
-__version__ = '2.1.3'
--- test.py
+++
@@ -1,6 +0,0 @@
-import nose
-from os import path
-
-file_path = path.abspath(__file__)
-tests_path = path.join(path.abspath(path.dirname(file_path)), "tests")
-nose.main(argv=[path.abspath(__file__), "--with-coverage", "--cover-erase", "--cover-package=nd2reader", tests_path])
