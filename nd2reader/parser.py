ae00cc1223fe31ca6333715e8812eb64419c86f1
unknown <wp2181@cumc.columbia.edu>
2019-09-10 17:53:25 -0400

    fix to allow reading of ND2-files with missing frames, due to e.g. skipping z-positions in one channel of a multichannel image; based on most recent commit of master

--- nd2reader/parser.py
+++ nd2reader/parser.py
@@ -283,7 +283,13 @@ class Parser(object):
         if np.any(image_data):
             return timestamp, Frame(image_data, metadata=self._get_frame_metadata())
 
-        raise NoImageError
+        # If a blank "gap" image is encountered, generate an empty frame (black image) of corresponding height and width to avoid 
+        # errors with ND2-files with missing frames.
+        else: 
+            empty_frame = np.zeros([height, width])
+            return timestamp, Frame(empty_frame, metadata=self._get_frame_metadata())     
+
+        # raise NoImageError # <-- delete?
 
     def _get_frame_metadata(self):
         """Get the metadata for one frame
