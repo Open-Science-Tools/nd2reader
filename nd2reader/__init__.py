75207959c78bb908028b5c0c9ee956ea94c15b21
Ruben Verweij <ruben@lighthacking.nl>
2021-04-23 12:53:29 +0200

    Fix #42: read version number from setup.py

--- nd2reader/__init__.py
+++ nd2reader/__init__.py
@@ -2,4 +2,12 @@ from os import path
 from nd2reader.reader import ND2Reader
 from nd2reader.legacy import Nd2
 
-__version__ = '3.2.3'
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+try:
+    __version__ = importlib_metadata.version(__name__)
+except:
+    print('Unable to read version number')
--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
-from nd2reader import __version__ as VERSION
+
+VERSION = '3.2.3'
 
 if __name__ == '__main__':
     setup(
