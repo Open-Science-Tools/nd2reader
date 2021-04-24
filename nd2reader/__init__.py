4d0da771ea52fd650f22744ab93f9a2cfe1d002d
Ruben Verweij <ruben@lighthacking.nl>
2021-04-24 15:31:49 +0200

    Test importlib import on travis

--- nd2reader/__init__.py
+++ nd2reader/__init__.py
@@ -2,10 +2,10 @@ from os import path
 from nd2reader.reader import ND2Reader
 from nd2reader.legacy import Nd2
 
-try:
-    import importlib.metadata as importlib_metadata
-except:
-    import importlib_metadata
+#try:
+import importlib.metadata as importlib_metadata
+#except:
+#    import importlib_metadata
 
 try:
     __version__ = importlib_metadata.version(__name__)
