af7ce894e98fabe5b30c97c79f95801d1784d9d8
Ruben Verweij <ruben@lighthacking.nl>
2021-04-24 15:39:36 +0200

    Install package on travis to correctly get version number

--- nd2reader/__init__.py
+++ nd2reader/__init__.py
@@ -2,12 +2,12 @@ from os import path
 from nd2reader.reader import ND2Reader
 from nd2reader.legacy import Nd2
 
-#try:
-import importlib.metadata as importlib_metadata
-#except:
-#    import importlib_metadata
+try:
+    import importlib.metadata as importlib_metadata
+except:
+    import importlib_metadata
 
 try:
     __version__ = importlib_metadata.version(__name__)
 except:
-    raise ValueError('Unable to read version number')
+    print('Unable to read version number')
